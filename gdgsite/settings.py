13e7d49507cc4540e4fdfd519f5a39b4149f5a11
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-08-31 01:09:21 +0530

    made models for project

--- gdgsite/settings.py
+++ gdgsite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
+    'events',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+		
+
+class Project(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField(max_length=300)
+	github_repo = models.CharField(max_length=100)
+	live_link = models.CharField(max_length=200)
+	#contributors, image are fetched from new tables
+
+	def __str__(self):
+		return self.name
+
+
+class Image(models.Model):
+	name = models.CharField(max_length=100)
+	project = models.ForeignKey(Project)
+	picture = models.ImageField(upload_to = 'projects', blank = True, null = True)
+	
+	def __str__(self):
+		return self.name
+
+
+class Contributors(models.Model):
+	name = models.CharField(max_length=100)
+	project = models.ForeignKey(Project)
+
+	def __str__(self):
+		return self.name
