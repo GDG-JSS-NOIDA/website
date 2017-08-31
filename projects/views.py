2ae918ebb95eb602dffa9284e874d1c8ddb3cd82
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-09-01 02:10:34 +0530

    added project home view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
+from django.http import *
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def DisplayProjects(request):
+	projects = Project.objects.all()
+	return render(request, 'projects/home.html', {'projects': projects})
