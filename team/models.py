237f83f903df14c561a2e4962fa04e12e3d784af
HargovindArora <hargovinds50@gmail.com>
2017-09-01 19:24:32 +0530

    Teams app updated

--- team/models.py
+++ team/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 from django.contrib import auth
-from accounts import models
 
 
 def upload_location(instance, filename):
@@ -9,14 +8,17 @@ def upload_location(instance, filename):
 
 
 class Team(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user_id = models.OneToOneField(settings.AUTH_USER_MODEL)
     name = models.CharField(max_length=30)
     status = models.CharField(max_length=50)
     pic = models.ImageField(upload_to=upload_location,
-                            null=False, blank=True, height_field="height_field"
+                            null=False, blank=True, height_field="height_field",
                             width_field="width_field")
-    github_link = models.CharField(max_length=50)
-    gmail = models.EmailField(max_length=250, description=_("Email Address"))
+    github_link = models.URLField(max_length=50)
+    email = models.EmailField(max_length=250)
     linkedin = models.CharField(
-        max_length=250, description=_("LinkedIn Account"))
-    description = models.CharField(max_length=250)
+        max_length=250)
+    description = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- team/views.py
+++ team/views.py
@@ -1,12 +1,60 @@
 from django.shortcuts import render
 from django.views import generic
 from . import models
+from . import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from braces.views import SelectRelatedMixin
 
 # Create your views here.
-
-
-class ListTeam(generic.ListView):
-    fields = ('name', 'user_id', 'github_link', 'gmail',
+class MainPage(generic.ListView,LoginRequiredMixin,SelectRelatedMixin):
+    template_name = 'team/main.html'
+    fields = ('name', 'github_link', 'email',
               'pic', 'description', 'linkedin', 'status')
-    model = Team
+    model = models.Team
+    context_object_name = 'member_details'
+
+class ListTeam(generic.ListView,SelectRelatedMixin):
+    fields = ('name', 'github_link', 'email',
+              'pic', 'description', 'linkedin', 'status')
+    model = models.Team
+    context_object_name = 'member_details'
     template_name = 'team/members.html'  # template name should be members.html
+
+class CreateMember(generic.CreateView,LoginRequiredMixin,SelectRelatedMixin):
+
+    model = models.Team
+    template_name = 'team/create_member.html'
+    success_url = reverse_lazy("team:main")
+    form_class = forms.MemberCreateForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+class EditMember(LoginRequiredMixin,generic.UpdateView,SelectRelatedMixin):
+
+    model = models.Team
+    template_name = 'team/edit_member.html'
+    success_url = reverse_lazy("team:main")
+    form_class = forms.MemberEditForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+class DeleteMember(generic.DeleteView,LoginRequiredMixin,SelectRelatedMixin):
+    model = models.Team
+    success_url = reverse_lazy("team:main")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id=self.request.userId.id)
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, "Post Deleted")
+        return super().delete(*args, **kwargs)
