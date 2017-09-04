5393b9179b72b0002ec517ee0be21f2618ebc29b
HargovindArora <hargovinds50@gmail.com>
2017-09-04 19:34:26 +0530

    Register app added

--- gdgsite/settings.py
+++ gdgsite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'projects',
     'events',
     'accounts',
-    'team'
+    'team',
+    'register'
 ]
 
 
@@ -129,4 +130,3 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
-
