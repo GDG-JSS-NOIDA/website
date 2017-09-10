1655f4edb3388e90ea2e07ff59645dd5712ab716
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-09-10 18:03:02 +0530

    events template rendered and included

--- gdgsite/settings.py
+++ gdgsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v6$8b53a&74w2_eyh&*g7^@dm6v!!=ra$cm)1l*adqxav&bt%0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
