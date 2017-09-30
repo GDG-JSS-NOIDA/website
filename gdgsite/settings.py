0ad4ac94f733e66aadc29323e58880782840cdff
Hargovind Singh Arora <hargovinds50@gmail.com>
2017-09-30 22:28:28 +0530

    Update settings.py

--- gdgsite/settings.py
+++ gdgsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v6$8b53a&74w2_eyh&*g7^@dm6v!!=ra$cm)1l*adqxav&bt%0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
