030c716d4281904bdf31849936322d32999620a6
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-10-01 17:39:33 +0530

    issue #58 fixed partially(on debug=false still persists)

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,14 +13,12 @@ from .models import *
 
 # Create your views here.
 def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
+    response = render(request, '404.html')
     response.status_code = 404
     return response
 
-def handler404(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
+def handler500(request):
+    response = render(request, '500.html')
     response.status_code = 500
     return response
 
