f208e127645517d54a8c406db16b0058e948b334
Hargovind Singh Arora <hargovinds50@gmail.com>
2017-10-01 22:55:55 +0530

    Code changes for error handling (#37)
    * #35 404 page displayed according to UI
    
    * Code changes for error handling
    
    * Update settings.py
    
    * issue #58 fixed partially(on debug=false still persists)

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,10 +5,22 @@ from django.contrib import auth
 from django.http import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models  import User
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 # from .forms import *
 from .models import *
 
 # Create your views here.
+def handler404(request):
+    response = render(request, '404.html')
+    response.status_code = 404
+    return response
+
+def handler500(request):
+    response = render(request, '500.html')
+    response.status_code = 500
+    return response
 
 def auth_view(request):
 	username = request.POST['username']
