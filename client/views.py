311e292d31a531e83ef628cf8eb5b199d8dff5c5
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-10-02 01:10:58 +0530

    issue #61 fix

--- client/views.py
+++ client/views.py
@@ -12,36 +12,38 @@ def index(request):
 
 
 def events(request):
-	event = Event.objects.all()
-	now = timezone.now()
-	ongoing = Event.objects.filter(start_date__lte=now, end_date__gte=now)
-	upcoming = Event.objects.filter(start_date__gte=now)
-	past = Event.objects.filter(end_date__lte=now)
-	full_list = []
-	for items in ongoing:
-		items.category = "ongoing"
-		full_list.append(items)
-	for items in upcoming:
-		items.category = "upcoming"
-		full_list.append(items)
-	for items in past:
-		items.category = "past"
-		full_list.append(items)
-	
-	context = {
-	"queryset1": full_list,
-	}
-	print(context)
-	return render(request, 'client/events.html', context)
+    event = Event.objects.all()
+    now = timezone.now()
+    ongoing = Event.objects.filter(start_date__lte=now, end_date__gte=now)
+    upcoming = Event.objects.filter(start_date__gte=now)
+    past = Event.objects.filter(end_date__lte=now)
+    full_list = []
+    for items in ongoing:
+        items.category = "ongoing"
+        full_list.append(items)
+    for items in upcoming:
+        items.category = "upcoming"
+        full_list.append(items)
+    for items in past:
+        items.category = "past"
+        full_list.append(items)
+
+    context = {
+        "queryset1": full_list,
+    }
+    print(context)
+    return render(request, 'client/events.html', context)
 
 
 def team(request):
-    team1 = Team.objects.filter(status = True).order_by('-year')
-    team2 = Team.objects.filter(status = False).order_by('-year')
+    team1 = Team.objects.filter(status=True).order_by('-year')
+    team2 = Team.objects.filter(status=False).order_by('-year')
     return render(request, 'client/team.html', {'teams1': team1, 'teams2': team2})
+
 
 def register(request):
     return render(request, 'client/register.html')
+
 
 def projects(request):
     project = Project.objects.all()
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from .views import *
-from django.conf.urls import (handler404,handler500)
-
-
+from django.conf.urls import (handler404, handler500)
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name = 'mainapp/home.html')),
-    url(r'^auth/$', auth_view, name = 'check'),
-    url(r'^logout/$', logout, name = 'logout'),
-    url(r'^projects/$', projects, name = 'projects'),
-    url(r'^events/$', events, name = 'events'),
-    url(r'^team/$', team, name = 'team'),
+    url(r'^$', TemplateView.as_view(template_name='mainapp/home.html')),
+    url(r'^auth/$', auth_view, name='check'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^projects/$', projects, name='projects'),
+    url(r'^events/$', events, name='events'),
+    url(r'^team/$', team, name='team'),
 
 ]
