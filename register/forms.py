951a109327c3c137e281e4f1089f38de225d4d66
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-12-29 20:00:57 +0530

    code sanitise

--- register/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.forms import ModelForm
-from register.models import Register
-
-class RegisterForm(ModelForm):
-
-    class Meta:
-        model = Register
-        fields = ('name', 'adm_no', 'email', 'contact_no')
--- ui/js/index.js
+++
@@ -1,3 +0,0 @@
-// TODO: auto resize message field to fit text
-
-//FORKED FROM https://codepen.io/jonathanglyn (which was originally forked from my Material Design Input)
